70576e84dcb3a93a8d08a4f34d8117d47df4fd49
miljanica <miljana.mimy95@gmail.com>
2023-08-29 23:49:10 +0200

    Adding SQL tables

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,89 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+------------------- PATIENTS --------------------------------
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+-- :name create-patient! :! :n
+-- :doc creates a new patient record
+INSERT INTO patients
+(healthCardNumber, name, surname, city, address, dateOfBirth, loyalityMember)
+VALUES (:healthCardNumber, :name, :surname, :city, :address, :dateOfBirth, :loyalityMember)
 
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
+-- :name get-user-by-id :? :1
+-- :doc get patient by id
+SELECT * FROM patients
+WHERE idPatient = :idPatient
 
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name update-patient! :! :n
+-- :doc update existing patient with id
+UPDATE patients
+SET healthCardNumber = :healthCardNumber, name = :name,
+    surname = :surname,city = :city, address = :address,
+    dateOfBirth = :dateOfBirth, loyalityMember = :loyalityMember
+WHERE idPatient = :idPatient
+
+-- :name get-patients :? :*
+-- :doc shows all patients with number of appointments
+SELECT *, COUNT(appointments.idAppointment) AS 'numberOfApps' FROM `patients`
+    JOIN appointments ON appointments.idPatient = patients.idPatient
+
+------------------- TREATMENTS --------------------------------
+
+-- :name create-treatment! :! :n
+-- :doc creates a new treatment record
+    INSERT INTO treatments
+(treatmentName, regularPrice, loyalityPrice)
+VALUES (:treatmentName, :regularPrice, :loyalityPrice)
+
+-- :name get-treatment-by-id :? :1
+-- :doc get treatment by id
+SELECT * FROM treatments
+WHERE idTreatment = :idTreatment
+
+-- :name update-treatment! :! :n
+-- :doc update existing treatment with id
+UPDATE treatments
+SET treatmentName = :treatmentName, regularPrice = :regularPrice,
+    loyalityPrice = :loyalityPrice
+WHERE idTreatment = :idTreatment
+
+-- :name get-treatments :? :*
+-- :doc shows all treatments
+SELECT * from treatments
+
+------------------- APPOINTMENTS --------------------------------
+
+-- :name create-appointment! :! :n
+-- :doc creates a new appointment record
+    INSERT INTO appointments
+(idPatient, idTreatment, date, time, price)
+VALUES (:patient, :treatment, :date, :time, :price)
+
+-- :name get-appointment-by-id :? :1
+-- :doc selects appointment by id
+SELECT * FROM appointments
+WHERE idAppointment = :idAppointment
+
+-- :name update-appointment! :! :n
+-- :doc update existing appointment with id
+UPDATE appointments
+SET idPatient = :idPatient, idTreatment = :idTreatment,
+    date = :date, time = :time, price = :price
+WHERE idAppointment = :idAppointment
+
+-- :name delete-appointment! :! :n
+-- :doc delete appointment with id
+DELETE FROM appointments
+WHERE idAppointment = :idAppointment
+
+-- :name get-appointments :? :*
+-- :doc shows all appointments
+SELECT * from appointments
+                  JOIN treatments ON appointments.idTreatment = treatments.idTreatment
+                  JOIN patients ON appointments.idPatient = patients.idPatient
+ORDER BY idAppointment ASC
+
+-- :name get-appointments-by-patient :? :*
+-- :doc shows all appointments that patient with id had
+SELECT *FROM appointments
+                 JOIN treatments ON appointments.idTreatment = treatments.idTreatment
+                 JOIN patients ON appointments.idPatient = patients.idPatient
+WHERE appointments.idPatient = :idPatient
+ORDER BY idAppointment ASC
