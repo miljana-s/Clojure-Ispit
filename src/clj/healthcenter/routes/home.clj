5779ef99279eb0688ed7321e8e3659111ce3ce6a
miljanica <miljana.mimy95@gmail.com>
2023-08-31 10:42:35 +0200

    Added create treatment

--- src/clj/healthcenter/routes/home.clj
+++ src/clj/healthcenter/routes/home.clj
@@ -1,12 +1,12 @@
 (ns healthcenter.routes.home
   (:require
-   [healthcenter.layout :as layout]
-   [healthcenter.db.core :as db]
-   [clojure.java.io :as io]
-   [struct.core :as st]
-   [healthcenter.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]))
+    [healthcenter.layout :as layout]
+    [healthcenter.db.core :as db]
+    [clojure.java.io :as io]
+    [struct.core :as st]
+    [healthcenter.middleware :as middleware]
+    [ring.util.response]
+    [ring.util.http-response :as response]))
 
 ;-------------------------- PAGE CONFIG -----------------------------------------
 (defn home-page [request]
@@ -35,10 +35,14 @@
            (select-keys flash [:id :errors]))))
 
 (defn treatments-page [request]
-  (layout/render request "treatments/treatments.html"))
+  (layout/render
+    request "treatments/treatments.html"
+    {:treatments (db/get-treatments)}))
 
-(defn createTreat-page [request]
-  (layout/render request "treatments/createTreat.html"))
+(defn createTreat-page [{:keys [flash] :as request}]
+  (layout/render
+    request "treatments/createTreat.html"
+    (select-keys flash [:treatmentName :regularPrice :loyalityPrice :errors])))
 
 (defn updateTreat-page [request]
   (layout/render request "treatments/updateTreat.html"))
@@ -78,7 +82,7 @@
     st/required
     {:message  "Card number must be positive number!"
      :validate (fn [num] (> (Integer/parseInt (re-find #"\A-?\d+" num)) 0))}
-    {:message "Card must have exactly five numbers!"
+    {:message  "Card must have exactly five numbers!"
      :validate (fn [num] (= (count num) 5))}]
    [:city
     st/required
@@ -88,6 +92,20 @@
     st/string]
    [:dateOfBirth
     st/required]])
+
+;Adding patient
+(def treatment-schema
+  [[:treatmentName
+    st/required
+    st/string]
+   [:regularPrice
+    st/required
+    {:message  "Price must be positive number!"
+     :validate (fn [num] (> (Integer/parseInt (re-find #"\A-?\d+" num)) 0))}]
+   [:loyalityPrice
+    st/required
+    {:message  "Price must be positive number!"
+     :validate (fn [num] (> (Integer/parseInt (re-find #"\A-?\d+" num)) 0))}]])
 
 ;Delete universal
 (def delete-schema
@@ -101,9 +119,13 @@
 (defn validate-patient [params]
   (first (st/validate params patient-schema)))
 
+;Treatment
+(defn validate-treatment [params]
+  (first (st/validate params treatment-schema)))
+
 ;Delete
-(defn validate-delete [params]
-  (first (st/validate params delete-schema)))
+
+
 
 ;-------------------------- METHODS -----------------------------------------
 
@@ -126,7 +148,6 @@
     (-> (response/found "/updatePatient")
         (assoc :flash (assoc params :errors errors)))
     (let [patient (db/get-patient-by-card params)]
-      (println patient)
       (if patient
         (-> (response/found "/updatePatient")
             (assoc :flash (assoc params :errors {:healthCardNumber "Patient with that health card number already exists!!"})))
@@ -136,16 +157,23 @@
 
 ;Delete patient
 (defn delete-patient! [{:keys [params]}]
-    (let [params-no-token (dissoc params :__anti-forgery-token)
-          patient (db/check-patient-treatments params-no-token)]
-      (println params-no-token)
-      (println patient)
-      (if patient
-        (-> (response/found "/deletePatient")
-            (assoc :flash (assoc params :errors {:id "You can't delete patient that has treatment record!!"})))
-        (do
-          (db/delete-patient! params)
-          (response/found "/patients")))))
+  (let [params-no-token (dissoc params :__anti-forgery-token)
+        patient (db/check-patient-treatments params-no-token)]
+    (if patient
+      (-> (response/found "/deletePatient")
+          (assoc :flash (assoc params :errors {:id "You can't delete patient that has treatment record!!"})))
+      (do
+        (db/delete-patient! params)
+        (response/found "/patients")))))
+
+;Adding new treatment
+(defn add-treatment! [{:keys [params]}]
+  (if-let [errors (validate-treatment params)]
+    (-> (response/found "/createTreatment")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/create-treatment! params)
+      (response/found "/treatments"))))
 
 ;-------------------------- ROUTING -----------------------------------------
 
@@ -158,6 +186,8 @@
    ["/addPatient" {:post add-patient!}]
    ["/editPatient" {:post update-patient!}]
    ["/removePatient" {:post delete-patient!}]
+
+   ["/addTreatment" {:post add-treatment!}]
 
    ; ---------- GET REQ ------------------------
    ["/" {:get home-page}]
